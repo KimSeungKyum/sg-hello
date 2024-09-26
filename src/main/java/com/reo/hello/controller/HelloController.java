701180694549c4e08a8007a8449d16a9d7edb64f
kyum <kyum@abc.com>
2024-09-26 17:15:01 +0900

    commit

--- src/main/java/com/reo/hello/controller/HelloController.java
+++ src/main/java/com/reo/hello/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/hello")
 	public String helloPage(Model model) {
-		model.addAttribute("greeting", "hello!  SG");
+		model.addAttribute("greeting", "ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ");
 		return "hello";
 	}
 }
